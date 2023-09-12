7683c748ba911458a345548c08108d6aa2ea0109
Fae-new <faluyiaji@gmail.com>
2023-09-12 16:30:17 -0700

    24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+/**
+ * jack_bauer - Print every minute of a 24-hour day.
+ *
+ *
+ * The function has no return value (void).
+ */
+
+
+void jack_bauer(void)
+{
+int i;
+
+for (i = 0; i <= 23; i++)
+{
+int j;
+
+for (j = 0; j < 60; j++)
+{
+
+putchar('0' + (i / 10));
+
+putchar('0' + (i % 10));
+
+putchar(':');
+
+putchar('0' + (j / 10));
+
+putchar('0' + (j % 10));
+
+putchar('\n');
+}
+}
+}
