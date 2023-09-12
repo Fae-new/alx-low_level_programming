308ef8b9efd8499a0767ac4dc2f10b7f541bf3da
Fae-new <faluyiaji@gmail.com>
2023-09-12 15:16:17 -0700

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+/**
+ * print_last_digit - Print the last digit of an integer.
+ *
+ * @n: The integer whose last digit is to be printed.
+ *
+ * Return: The last digit of 'n'.
+ */
+
+
+int print_last_digit(int n)
+{
+
+return (n % 10);
+
+
+}
