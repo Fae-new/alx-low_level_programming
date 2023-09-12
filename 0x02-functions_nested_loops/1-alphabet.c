c25d72505e4201ccfde581138b53e4dfc457f386
Fae-new <faluyiaji@gmail.com>
2023-09-12 12:48:02 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+
+_putchar("\n")
+return (0);
+}
