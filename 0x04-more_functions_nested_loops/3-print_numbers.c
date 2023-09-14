a12a506985d472a0d0fb72f4c9d576a27c6b0013
Fae-new <faluyiaji@gmail.com>
2023-09-14 14:13:38 -0700

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * print_numbers - Prints numbers from 0 to 9.
+ *
+ * This function prints the numbers from 0 to 9, each separated by a space.
+ * It does not take any parameters.
+ *
+ * Return: This function does not return a value (void).
+ */
+void print_numbers(void)
+{
+
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n')
+}
