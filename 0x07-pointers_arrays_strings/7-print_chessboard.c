0d7015916bdfdc0bc82a3e0e9b41139152f64330
Fae-new <faluyiaji@gmail.com>
2023-09-25 15:10:48 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - Print a chessboard.
+ *
+ * @a: Pointer to a chessboard represented as an array of arrays.
+ */
+
+
+void print_chessboard(char (*a)[8])
+{
+
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+putchar('\n');
+}
+}
