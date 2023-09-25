1f787fab7e961d64473cc185455f0e0c1c39df26
Fae-new <faluyiaji@gmail.com>
2023-09-25 14:25:51 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+/**
+ * _strpbrk - Find the first character from 'accept' in 's'.
+ *
+ * @s: String to search.
+ * @accept: Set of characters to look for.
+ *
+ * Return: Pointer to the first matching character, or NULL if not found.
+ */
+
+
+
+char *_strpbrk(char *s, char *accept)
+{
+
+char *start = accept;
+
+while (*s != '\0')
+{
+
+accept = start;
+
+while (*accept != '\0')
+{
+if (*s == *accept)
+return (s);
+accept++;
+}
+
+s++;
+}
+
+return (NULL);
+}
