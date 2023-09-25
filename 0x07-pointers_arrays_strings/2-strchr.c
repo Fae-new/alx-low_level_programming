af36fc94b6363de6d84d291860d5b6f4ee6b7f5d
Fae-new <faluyiaji@gmail.com>
2023-09-25 13:50:08 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * _strchr - Locate the first occurrence of a character in a string.
+ *
+ * @s: The string to search in.
+ * @c: The character to locate.
+ *
+ * Return: Pointer to the first occurrence of 'c' in 's' or NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\n')
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (NULL);
+}
