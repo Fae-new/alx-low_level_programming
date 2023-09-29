529e6e9057f900d9019677b4305e326dc73ae55e
Fae-new <faluyiaji@gmail.com>
2023-09-28 22:00:04 -0700

    prime

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,34 @@
+#include "main.h"
+
 /**
- * is_prime_number - detects if an input number is a prime number.
+ * power_operation - returns the natural square root of a number.
  * @n: input number.
- * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ * @c: iterator.
+ * Return: square root or -1.
  */
-int is_prime_number(int n)
+int power_operation(int n, int c)
 {
-if (n == 0)
-return (0);
-if (n < 0)
-return (0);
-if (n == 1)
-return (0);
-return (is_prime(n, 2));
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
