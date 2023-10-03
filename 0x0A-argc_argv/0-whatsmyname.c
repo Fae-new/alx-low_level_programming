b4866aaa00c42577175d267d107fb82c0389acc6
Fae-new <faluyiaji@gmail.com>
2023-10-02 19:21:41 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints its name, and adapts to new names without recompilation.
+ * The path before the program name is retained.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char **argv)
+{
+
+    printf("%s", argv[0]);
+
+    return (0);
+}
