2c77f711178a964efbdaf6a0419caa4bf90564e8
Fae-new <faluyiaji@gmail.com>
2023-10-02 19:48:00 -0700

    2args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,3 +1,15 @@
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array containing command-line arguments
+ *
+ * Description: Prints each command-line argument on a new line,
+ * including the program name.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 #include <stdio.h>
 
 int main(int argc, char **argv)
