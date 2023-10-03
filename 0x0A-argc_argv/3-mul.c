cf582bc6d5caa0d2e413f72277523b1cba97626f
Fae-new <faluyiaji@gmail.com>
2023-10-02 20:08:32 -0700

    3mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@
 #include <stdlib.h>
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        printf("Error");
-        return (1);
-    }
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 
-    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
-    return (0);
+return (0);
 }
