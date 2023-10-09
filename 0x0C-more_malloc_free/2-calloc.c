b46213d28e5dafcc297becb410711d581c3eca4e
Fae-new <faluyiaji@gmail.com>
2023-10-08 21:58:37 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,36 +1,29 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * _calloc - Allocates memory for an array, initializes to zero.
- * @nmemb: Number of elements in the array.
- * @size: Size of each element in bytes.
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
  *
- * Description: Allocates memory for an array of @nmemb elements
- * Return: Pointer to allocated and initialized memory, or NULL on failure.
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
-
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *p;
+	unsigned int i;
 
-int i;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-void *ptr = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-if (ptr == NULL)
-return (NULL);
+	if (p == NULL)
+		return (NULL);
 
-if (nmemb == 0 || size == 0)
-return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
-char *char_ptr = ptr;
-for (i = 0; i < nmemb * size; i++)
-{
-char_ptr[i] = 0;
-}
-
-return (ptr);
+	return (p);
 }
