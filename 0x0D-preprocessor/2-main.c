85a5d1c563ddb1d47052c199048fc548842d2443
Fae-new <faluyiaji@gmail.com>
2023-10-10 14:34:25 -0700

    sum

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+printf("%s", __FILE__);
+
+return (0);
+}
