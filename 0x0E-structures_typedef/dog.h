c32c8dc4f208f77e9d2e9c921af508b186cb70ee
Fae-new <faluyiaji@gmail.com>
2023-10-10 15:54:15 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Represents information about a dog.
+ * @name: Pointer to the dog's name.
+ * @age: Age of the dog.
+ * @owner: Pointer to the owner's name.
+ *
+ * Description: This structure contains information about a dog.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
