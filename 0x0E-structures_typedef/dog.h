fa1e0bc7f5b8ab50a3feddb8f27a39647912e852
Fae-new <faluyiaji@gmail.com>
2023-10-10 17:01:20 -0700

    print

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,13 @@
  *
  * Description: This structure contains information about a dog.
  */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 #endif
