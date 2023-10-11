4f03db7fc8aecbdc8d3665b77c2b990706a53609
Fae-new <faluyiaji@gmail.com>
2023-10-10 17:34:56 -0700

    newdog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * dog_t - Typedef for struct dog
  */
 
-typedef struct dog dog_t;
 
 /**
  * struct dog - Represents information about a dog.
@@ -17,12 +16,12 @@ typedef struct dog dog_t;
  * Description: This structure contains information about a dog.
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
