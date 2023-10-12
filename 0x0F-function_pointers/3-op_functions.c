3167e45d13d5d0c4e358d45452bb91386609e356
Fae-new <faluyiaji@gmail.com>
2023-10-11 21:52:52 -0700

    done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Description: Returns the sum of two integers, @a and @b.
+ *
+ * Return: Sum of @a and @b.
+ */
+
+/**
+ * op_sub - Subtracts two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Description: Returns the difference between two integers, @a and @b.
+ *
+ * Return: Difference between @a and @b.
+ */
+
+/**
+ * op_mul - Multiplies two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Description: Returns the product of two integers, @a and @b.
+ *
+ * Return: Product of @a and @b.
+ */
+
+/**
+ * op_div - Divides two integers.
+ * @a: First integer.
+ * @b: Second integer (non-zero).
+ *
+ * Description: Returns the result of dividing @a by @b.
+ *
+ * Return: Result of @a divided by @b.
+ */
+
+/**
+ * op_mod - Computes the remainder of division of two integers.
+ * @a: First integer.
+ * @b: Second integer (non-zero).
+ *
+ * Description: Returns the remainder of dividing @a by @b.
+ *
+ * Return: Remainder of @a divided by @b.
+ */
+
+
+
+int op_add(int a, int b) 
+{ 
+return (a + b);
+}
+
+int op_sub(int a, int b) 
+{ 
+return (a - b);
+}
+
+int  op_mul(int a, int b) 
+{ 
+return (a * b);
+}
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
